3893a0ad43e983d76a894d2da120c8170d2d288f
闫威 <779463900@qq.com>
2021-12-20 20:07:30 +0800

    搜索

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,7 @@
 <template>
   <view>
+    <!-- 自定义搜索组件 -->
+    <my-search @click="gotoSearch"></my-search>
     <view class="scroll-view-container">
       <!-- 左 -->
       <scroll-view scroll-y="true" :style="{height: wh + 'px'}" class="left-scroll-view">
@@ -43,7 +45,7 @@
     },
     onLoad() {
       const sysInfo = uni.getSystemInfoSync()
-      this.wh = sysInfo.windowHeight
+      this.wh = sysInfo.windowHeight - 50
       this.getCateList()
     },
     methods: {
@@ -66,6 +68,11 @@
       gotoGoodsList(item) {
         uni.navigateTo({
           url: '/subpkg/goods_list/goods_list?cid=' + item.cat_id
+        })
+      },
+      gotoSearch() {
+        uni.navigateTo({
+          url: '/subpkg/serach/serach'
         })
       }
     }
