d868bb09b6f8eb33082e20aac2adf46e9d0cbd0c
闫威 <779463900@qq.com>
2021-12-25 12:36:58 +0800

    支付

--- store/user.js
+++ store/user.js
@@ -5,7 +5,12 @@ export default {
   // state 数据
   state: () => ({
     // 收货地址
-    address: JSON.parse(uni.getStorageSync('address') || '{}')
+    address: JSON.parse(uni.getStorageSync('address') || '{}'),
+    token: uni.getStorageSync('token') || '',
+    // 用户的基本信息
+    userinfo: JSON.parse(uni.getStorageSync('userinfo') || '{}'),
+    // 重定向的 object 对象 { openType, from }
+    redirectInfo: null
   }),
 
   // 方法
@@ -20,6 +25,26 @@ export default {
     saveAddressToStorage(state) {
       uni.setStorageSync('address', JSON.stringify(state.address))
     },
+    // 更新用户的基本信息
+    updateUserInfo(state, userinfo) {
+      state.userinfo = userinfo
+      // 通过 this.commit() 方法，调用 m_user 模块下的 saveUserInfoToStorage 方法，将 userinfo 对象持久化存储到本地
+      this.commit('m_user/saveUserInfoToStorage')
+    },
+    saveUserInfoToStorage(state) {
+      uni.setStorageSync('userinfo', JSON.stringify(state.userinfo))
+    },
+    updateToken(state, token) {
+      state.token = token
+      this.commit('m_user/saveTokenToStorage')
+    },
+    saveTokenToStorage(state) {
+      uni.setStorageSync('token', state.token)
+    },
+    // 更新重定向的信息对象
+    updateRedirectInfo(state, info) {
+      state.redirectInfo = info
+    }
   },
 
   // 数据包装器
